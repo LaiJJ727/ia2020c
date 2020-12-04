e5b333439395e3e5dbebcf0e036c319a94ccbc98
B200019A <B200019A@sc.edu.my>
2020-12-04 22:54:10 +0800

    dec4

---
+++ java/index.js
@@ -0,0 +1,26 @@
+var i=10;
+
+if(i===10){
+    console.log("i is more than 10");
+} else if(i=="10"){
+    console.log("1 is equal to string ten");
+}
+else{
+   console.log("i is less than 10");
+}
+
+for(var i=0; i<3; i++){
+    console.log(i);
+}
+
+for(var y=5; y>0; y--){
+    console.log("y is ="+ y);
+}
+for(var i=0; i<100; i+=5){
+    console.log(i);
+}
+var w=0;
+for(var x=0; x<=5; x++){
+     w=w+x;
+    console.log("x="+w);
+}
