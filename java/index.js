c08f3daca02d60cd9a6db19bdeb34a3e24e189b0
B200019A <B200019A@sc.edu.my>
2020-12-18 11:20:12 +0800

    1812

--- java/index.js
+++ java/index.js
@@ -24,3 +24,26 @@ for(var x=0; x<=5; x++){
      w=w+x;
     console.log("x="+w);
 }
+//exercise 1
+function addvalue(a,b){
+    console.log(a+b);
+}
+addvalue(2,4);
+
+//exercise2
+
+function calculatePuppyAge(age,sex){
+    var puppyAge = 7*age;
+    console.log("Your doggie age is"+ puppyAge+"compare to human")
+}
+
+calculatePuppyAge(10,",female");
+calculatePuppyAge(2,",male");
+
+//exercise3
+function minusvalue(a,b){
+    console.log(a-b);
+}
+minusvalue(9,2);
+
+//exercise4
---
+++ java/try.js
@@ -0,0 +1,8 @@
+var x = userAge;
+function calculatePuppyAge(age){
+    if(age<=18){
+     console
+    } 
+
+}
+calculatePuppyAge(age)
